3d5d9d15349f39ee41fe62fedb7cb62a38d918ea
Hyejin Kim <cshjkim@gmail.com>
2020-01-22 00:12:02 -0800

    coding burger.js before test

--- models/burger.js
+++ models/burger.js
@@ -3,3 +3,26 @@
 // create code that will call the ORM functions using burger specific input for the ORM.
 
 //export
+
+var orm = require("../config/orm.js");
+var burgers = {
+	selectAll: function(cb){
+		orm.selectAll("burgers", function(res){
+			cb(res);
+		});
+	},
+
+	insertOne: function(cols,vals,cb){
+		orm.insertOne("burgers", cols, vals, function(res){
+			cb(res);
+		});
+	},
+
+	updateOne: function(objColVals, condition, cb){
+		orm.updateOne("burgers", objColVals, condition, function(res){
+			cb(res);
+		});
+	},
+};
+
+module.exports = burgers;
