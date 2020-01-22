bb2070b84a2c04a93ec38b8f3357d134d69ab9e1
Hyejin Kim <cshjkim@gmail.com>
2020-01-22 00:12:49 -0800

    first coding burger controller before testing

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,41 @@
-// import express
 
-// import burger.js
+// import express
+var express = require('express');
+var router = express.Router();
+
+// import burger.js.
+var burger = require('../models/burger.js');
 
 //create the router for the app and export
+router.get('/', function(req, res) {
+  burger.selectAll(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    // console.log(hbsObject);
+    res.render('index', hbsObject);
+  });
+});
+
+router.post('/api/burgers', function(req, res) {
+  burger.insertOne([
+    'burger_name'
+  ], [
+    req.body.burger_name
+  ], function(data) {
+    res.redirect('/');
+  });
+});
+
+router.put('/api/burgers/:id', function(req, res) {
+  var condition = 'id = ' + req.params.id;
+
+  burger.updateOne({
+    devoured: true
+  }, condition, function(data) {
+    res.redirect('/');
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
